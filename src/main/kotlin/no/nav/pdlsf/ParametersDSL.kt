9dbfd3afb090c41d775872d1eec7f9c620b0bf7a
Tommy Kristiansen <tommy.kristiansen@nav.no>
2020-09-11 10:26:06 +0200

    reverted to using cache

--- src/main/kotlin/no/nav/pdlsf/ParametersDSL.kt
+++ src/main/kotlin/no/nav/pdlsf/ParametersDSL.kt
@@ -187,7 +187,7 @@ data class Vault(
 
 data class EnvVar(
         // salesforce details
-    val sfInstanceType: String = getEnvOrDefault("SF_INSTTYPE", "SCRATCH").toUpperCase(), // TODO:: Default to MOCK instead of SCRATCH
+    val sfInstanceType: String = getEnvOrDefault("SF_INSTTYPE", "MOCK").toUpperCase(), // TODO:: Default to MOCK instead of SCRATCH
     val sfVersion: String = getEnvOrDefault("SF_VERSION", "v48.0"),
 
         // kafka details
@@ -235,53 +235,3 @@ fun Vault.credentials(): String = Base64.getEncoder().encodeToString("$kafkaUser
 
 internal fun getStringFromResource(path: String) =
         Params::class.java.getResourceAsStream(path).bufferedReader().use { it.readText() }
-
-/*
-object ParamsFactory {
-    val p: Params by lazy { Params() }
-}
-
-// TODO:: Read parameters from vault
-data class Params(
-        // kafka details
-    val kafkaBrokers: String = System.getenv("KAFKA_BROKERS")?.toString() ?: "localhost:9092",
-    val kafkaSchemaRegistry: String = System.getenv("KAFKA_SCREG")?.toString() ?: "",
-    val kafkaClientID: String = System.getenv("KAFKA_CLIENTID")?.toString() ?: "sf-pdl-pilot-default",
-    val kafkaProducerTimeout: Int = System.getenv("KAFKA_PRODUCERTIMEOUT")?.toInt() ?: 31_000,
-    val kafkaSecurity: String = System.getenv("KAFKA_SECURITY")?.toString()?.toUpperCase() ?: "FALSE",
-    val kafkaSecProt: String = System.getenv("KAFKA_SECPROT")?.toString() ?: "",
-    val kafkaSaslMec: String = System.getenv("KAFKA_SASLMEC")?.toString() ?: "",
-    val kafkaUser: String = ("/var/run/secrets/nais.io/serviceuser/username".readFile() ?: "username"),
-    val kafkaPassword: String = ("/var/run/secrets/nais.io/serviceuser/password".readFile() ?: "password"),
-    val kafkaTopicSf: String = System.getenv("KAFKA_TOPIC_SF")?.toString() ?: "",
-
-        // other details
-    val httpsProxy: String = System.getenv("HTTPS_PROXY") ?: "",
-    val msBetweenWork: Long = System.getenv("MS_BETWEEN_WORK")?.toLong() ?: 5 * 60 * 100,
-    val pdlGraphQlApiKey: String = ("/var/run/secrets/nais.io/apigw/pdl-api/x-nav-apiKey".readFile() ?: ""),
-    val pdlGraphQlUrl: String = System.getenv("PDL_GRAPHQL_URL") ?: "",
-    val stsApiKey: String = ("/var/run/secrets/nais.io/apigw/security-token-service-token/x-nav-apiKey".readFile() ?: ""),
-    val stsUrl: String = System.getenv("STS_REST_URL") ?: "",
-
-    val pilotList: String = ("/var/run/secrets/nais.io/vault/pilotlist".readFile() ?: getStringFromResource("/pilotlist"))
-)
-
-fun Params.credentials(): String = Base64.getEncoder().encodeToString("$kafkaUser:$kafkaPassword".toByteArray(Charsets.UTF_8))
-
-fun Params.kafkaSecurityEnabled(): Boolean = kafkaSecurity == "TRUE"
-
-fun Params.kafkaSecurityComplete(): Boolean =
-        kafkaSecProt.isNotEmpty() && kafkaSaslMec.isNotEmpty() && kafkaUser.isNotEmpty() && kafkaPassword.isNotEmpty()
-
-internal fun String.readFile(): String? =
-        try {
-            File(this).readText(Charsets.UTF_8)
-        } catch (err: FileNotFoundException) {
-            null
-        }
-
-internal fun getStringFromResource(path: String) =
-        ParamsFactory::class.java.getResourceAsStream(path).bufferedReader().use { it.readText() }
-
-
-*/
